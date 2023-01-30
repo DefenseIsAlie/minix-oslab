6c3beb006c38407f87c0924f22a7d60b4069f2e3
DefenseIsAlie <200010021@iitdh.ac.in>
2023-01-30 14:12:41 +0530

    Added SJF scheduler

--- Lab4/include/SJF.hpp
+++ Lab4/include/SJF.hpp
@@ -1,26 +1,83 @@
-#ifndef SJF
-#define SJF
-
 #include <vector>
 
-#include "include.hpp"
 
-class SJF
-{
-private:
-    /* data */
+class SJF{
+
 public:
-    SJF(std::vector<>);
-    ~SJF();
+    std::vector<processBlock> processQ;
+    int currTime = 0;
+    int isdone = 0;
+
+
+    SJF(std::vector<processBlock> processQue);  
+
+    void doJob(processBlock& job);
+
+    processBlock pickNextJob();
+
+    void simulate();
 };
 
-SJF::SJF(/* args */)
-{
+SJF::SJF(std::vector<processBlock> processQue){
+    this->processQ = processQue;
 }
 
-SJF::~SJF()
-{
+void SJF::doJob(processBlock& job){
+    this->currTime+= job.TotalBurst + job.TotalIoTime;
+
+    for (int i = 0; i < this->processQ.size(); i++){
+        
+        if (this->processQ[i].arrivalTime == job.arrivalTime && this->processQ[i].TotalBurst == job.TotalBurst && this->processQ[i].TotalIoTime == job.TotalIoTime) {
+            this->processQ.erase(this->processQ.begin() + i);
+            return;
+        }
+        
+    }
+
 }
 
+// 0 for successfully picking next job
+// 1 for empty processQ
+// -1 fail due to no arrival never returns -1
+processBlock SJF::pickNextJob(){
 
-#endif
+
+    processBlock ret("-1");
+
+    if (this->processQ.empty()){
+       this->isdone = 1;
+       return ret;
+    }
+    
+
+    this->isdone = -1;
+
+    processBlock minJob = this->processQ[0];
+
+    schedule:
+    for (int i = 0; i < this->processQ.size(); i++){
+        
+        if (this->currTime > this->processQ[i].arrivalTime && (this->processQ[i].TotalBurst + this->processQ[i].TotalIoTime) <= (minJob.TotalBurst + minJob.TotalIoTime)){
+            minJob = this->processQ[i];
+            this->isdone = 0;
+        }
+    }
+
+    if (this->isdone ==-1 && !this->processQ.empty()){
+        this->currTime+=500;
+        goto schedule;
+    }
+    
+    return minJob;
+}
+
+void SJF::simulate(){
+    processBlock currentJob = this->pickNextJob();
+
+    while (!isdone){
+        this->doJob(currentJob);
+
+        currentJob = this->pickNextJob();
+    }
+    
+}
