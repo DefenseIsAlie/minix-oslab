9057fd883e7d2ffd892f9b85cb628719477716e2
DefenseIsAlie <200010021@iitdh.ac.in>
2023-01-30 14:14:10 +0530

    Added main file

--- Lab4/src/main.cpp
+++ Lab4/src/main.cpp
@@ -5,6 +5,7 @@
 
 #include "../include/include.hpp"
 #include "../include/SJF.hpp"
+#include "../include/RR.hpp"
 
 int main(int argc, char **argv){
     std::vector<std::string> input;
@@ -14,15 +15,30 @@ int main(int argc, char **argv){
     }
 
     std::vector<processBlock> processQ;
-    
+    std::vector<processBlock> processQ2;
     for (std::string line : input){
         
         processBlock blk(line);
         processQ.push_back(blk);
         
     }
+    processQ2 = processQ;
 
+    // SJF sjf(processQ);
 
+    // sjf.simulate();
+
+    std::cout << "Started rr" << "\n";
+
+    RR rr(processQ);
+    rr.simulate();
+
+    std::cout << "Finished rr" << "\n";
+    // std::vector<processBlock> processQ3;
+    // processQ3.push_back(processQ2[0]);
+
+    // RR rr(processQ3);
+    // rr.simulate();
 
     return 0;
 
